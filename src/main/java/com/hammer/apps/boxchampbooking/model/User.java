ce01fc5329041143b987a26bd0f679941b44b0cd
mqqx <hmmrmx@gmail.com>
2019-04-17 23:25:09 +0200

    ignore password sending/printing via json and toString

--- src/main/java/com/hammer/apps/boxchampbooking/model/User.java
+++ src/main/java/com/hammer/apps/boxchampbooking/model/User.java
@@ -1,6 +1,8 @@
 package com.hammer.apps.boxchampbooking.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,12 +10,14 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@ToString(exclude = "password")
 public class User {
 
 	@Id
 	@GeneratedValue
 	private int userId;
 	private String username;
+	@JsonIgnore
 	private String password;
 	private boolean enabled;
 }
