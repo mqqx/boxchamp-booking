0feca4afc743389d4c06475a019038ab0f4d0211
mqqx <github@mqqx.dev>
2019-04-17 20:30:52 +0200

    added application shutdown after booking is finished to prevent multiple instances or blocked port due to still running instances of this app updated readme

--- src/main/java/com/hammer/apps/boxchampbooking/Application.java
+++ src/main/java/com/hammer/apps/boxchampbooking/Application.java
@@ -34,7 +34,7 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args).close();
 	}
 
 	@Bean
