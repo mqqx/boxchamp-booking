2541e35908965ae0a6e4610606830c5de6b8217f
mqqx <github@mqqx.dev>
2019-04-18 18:27:55 +0200

    added more class types

--- src/main/java/com/hammer/apps/boxchampbooking/model/ClassType.java
+++ src/main/java/com/hammer/apps/boxchampbooking/model/ClassType.java
@@ -1,10 +1,15 @@
 package com.hammer.apps.boxchampbooking.model;
 
 public enum ClassType {
+	ENDURANCE("Endurance"),
 	FITNESS("Fitness"),
 	GYMNASTICS("Gymnastics"),
+	MASTERS("Masters"),
+	MOBILITY("Mobility"),
 	OLYMPIC_WEIGHTLIFTING("Olympic Weightlifting"),
-	ENDURANCE("Endurance");
+	OPEN_BOX("Open Box"),
+	PERFORMANCE("Performance"),
+	RUNNING("Running");
 
 	private String name;
 
