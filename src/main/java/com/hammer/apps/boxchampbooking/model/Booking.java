0d1546ad3f5ff582d4f5e078fb04ecb500cbec70
mqqx <hmmrmx@gmail.com>
2019-04-24 10:33:35 +0200

    added unique constraint as there can only be one booking for each day of week per user

--- src/main/java/com/hammer/apps/boxchampbooking/model/Booking.java
+++ src/main/java/com/hammer/apps/boxchampbooking/model/Booking.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
@@ -16,6 +19,7 @@ import java.time.LocalTime;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "dayOfWeek"})})
 public class Booking {
 	@Id
 	@GeneratedValue
@@ -24,7 +28,11 @@ public class Booking {
 	@JsonIgnore
 	@ManyToOne
 	private User user;
+
+	@NotNull
 	private DayOfWeek dayOfWeek;
+
+	@NotNull
 	private ClassType classType;
 	private LocalTime time;
 
