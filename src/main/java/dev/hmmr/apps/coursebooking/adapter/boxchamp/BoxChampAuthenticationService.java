a7b9e04650e30420070d88094e7633fcbb6bfe01
mqqx <github@mqqx.dev>
2020-03-29 22:55:18 +0200

    * extract constants into configurable properties

--- src/main/java/dev/hmmr/apps/coursebooking/adapter/boxchamp/BoxChampAuthenticationService.java
+++ src/main/java/dev/hmmr/apps/coursebooking/adapter/boxchamp/BoxChampAuthenticationService.java
@@ -2,12 +2,12 @@ package dev.hmmr.apps.coursebooking.adapter.boxchamp;
 
 import dev.hmmr.apps.coursebooking.model.User;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
@@ -18,28 +18,31 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 
 import static java.lang.String.format;
+import static org.springframework.http.HttpHeaders.COOKIE;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 import static org.springframework.util.CollectionUtils.isEmpty;
 
-@Service
-@AllArgsConstructor
 @Slf4j
+@Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties(BoxChampProperties.class)
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 class BoxChampAuthenticationService {
-    private static final String QUERY_PARAM_EMAIL = "txEmail";
-    private static final String QUERY_PARAM_PASSWORD = "txPassword";
-    private static final String LOGIN_PATH = "home";
     private static final String USER_COULD_NOT_BE_AUTHENTICATED_MESSAGE = "User %s could not be authenticated. Please check username/password combination.";
 
     BoxChampHttpClient boxChampHttpClient;
+    BoxChampProperties boxChampProperties;
 
     HttpEntity<HttpHeaders> authenticate(User user) {
         HttpEntity<MultiValueMap<String, String>> request = authenticationHeaders(user);
-        ResponseEntity<String> loginResponse = boxChampHttpClient.post(LOGIN_PATH, request);
+        String loginPath = boxChampProperties.getLoginPath();
+        ResponseEntity<String> loginResponse = boxChampHttpClient.post(loginPath, request);
 
         String token = extractAndVerifySessionCookie(user, loginResponse);
 
         HttpHeaders getHeaders = new HttpHeaders();
-        getHeaders.add(HttpHeaders.COOKIE, token);
+        getHeaders.add(COOKIE, token);
 
         log.info("User {} logged in to box champ", user.getId());
 
@@ -48,7 +51,8 @@ class BoxChampAuthenticationService {
 
     private String extractAndVerifySessionCookie(User user, ResponseEntity<String> loginResponse) {
         String sessionCookie = "";
-        List<String> setCookieValues = loginResponse.getHeaders().get(HttpHeaders.SET_COOKIE);
+        List<String> setCookieValues = loginResponse.getHeaders().get(SET_COOKIE);
+
         if (!isEmpty(setCookieValues)) {
             sessionCookie = setCookieValues.get(setCookieValues.size() - 1);
         }
@@ -57,6 +61,7 @@ class BoxChampAuthenticationService {
                 && loginResponse.getBody().contains("login_errors");
 
         boolean hasAuthenticationFailed = hasLoginFailed || StringUtils.isEmpty(sessionCookie);
+
         if (hasAuthenticationFailed) {
             String userCouldNotBeAuthenticatedMessage = format(USER_COULD_NOT_BE_AUTHENTICATED_MESSAGE, user.getUsername());
             throw new BadCredentialsException(userCouldNotBeAuthenticatedMessage);
@@ -67,11 +72,14 @@ class BoxChampAuthenticationService {
 
     private HttpEntity<MultiValueMap<String, String>> authenticationHeaders(User user) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setContentType(APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> parts = new LinkedMultiValueMap<>();
-        parts.add(QUERY_PARAM_EMAIL, user.getUsername());
-        parts.add(QUERY_PARAM_PASSWORD, user.getPassword());
+        String queryParamEmail = boxChampProperties.getQueryParamEmail();
+        String queryParamPassword = boxChampProperties.getQueryParamPassword();
+
+        parts.add(queryParamEmail, user.getUsername());
+        parts.add(queryParamPassword, user.getPassword());
 
         return new HttpEntity<>(parts, headers);
     }
--- src/test/java/dev/hmmr/apps/coursebooking/adapter/boxchamp/BoxChampAuthenticationServiceTest.java
+++ src/test/java/dev/hmmr/apps/coursebooking/adapter/boxchamp/BoxChampAuthenticationServiceTest.java
@@ -28,14 +28,21 @@ import static org.mockito.Mockito.when;
 class BoxChampAuthenticationServiceTest {
 
     private static final String SESSION_COOKIE = "cisession=O0gyK4f5kmbibsFkcj%2FwjwkPSTAVmjodCDffoJB4BUbcmX1qkIgt%2FlccNJ0X1beISUhljR1gUU3RBvxKk0iEpm4Ke3aKTCE8IbXHbX8QP%2FwpTHIGz8WuHHf5Hw0Llz17rJIUCUNHZvno3L18rPr0A4cSBBzAXmdPWnoNkqK8GrMlCJs7AvWgDRIMaJSmqp7%2F6ro8UAHF0zZr5eBtcYBzkeQRZHXwJY2EAKULjcNRJB3z6MIucXxiXzLzdgEEC5CRYLcxuMz%2Bwn54y23VHrLdMF6WuW%2FM4ReFv4FBB%2BnhctaHD11TKBjWwU%2FZ2jCO0GgD9jVTXWL8ccbRgxOJyAcvYQ%3D%3D; expires=Wed, 21-Aug-2019 21:13:31 GMT; Max-Age=21600; path=/";
-    BoxChampAuthenticationService loginService;
+    private static final String QUERY_PARAM_EMAIL = "txEmail";
+    private static final String QUERY_PARAM_PASSWORD = "txPassword";
+    private static final String LOGIN_PATH = "home";
 
     @Mock
     BoxChampHttpClient boxChampHttpClient;
 
+    @Mock
+    BoxChampProperties boxChampProperties;
+
+    BoxChampAuthenticationService sut;
+
     @BeforeEach
     void setUp() {
-        loginService = new BoxChampAuthenticationService(boxChampHttpClient);
+        sut = new BoxChampAuthenticationService(boxChampHttpClient, boxChampProperties);
     }
 
     @Test
@@ -43,24 +50,29 @@ class BoxChampAuthenticationServiceTest {
     void authenticate_ok() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
         String username = "username";
         String password = "password";
+
         MultiValueMap<String, String> parts = new LinkedMultiValueMap<>();
-        parts.add("txEmail", username);
-        parts.add("txPassword", password);
+        parts.add(QUERY_PARAM_EMAIL, username);
+        parts.add(QUERY_PARAM_PASSWORD, password);
 
         ResponseEntity<String> responseEntity = ResponseEntity
                 .ok()
                 .header(HttpHeaders.SET_COOKIE, SESSION_COOKIE)
                 .body("<!doctype html><html />");
         HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(parts, headers);
-        when(boxChampHttpClient.post("home", httpEntity)).thenReturn(responseEntity);
+        when(boxChampHttpClient.post(LOGIN_PATH, httpEntity)).thenReturn(responseEntity);
+        when(boxChampProperties.getQueryParamEmail()).thenReturn(QUERY_PARAM_EMAIL);
+        when(boxChampProperties.getQueryParamPassword()).thenReturn(QUERY_PARAM_PASSWORD);
+        when(boxChampProperties.getLoginPath()).thenReturn(LOGIN_PATH);
 
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
 
-        HttpEntity<HttpHeaders> sut = loginService.authenticate(user);
+        HttpEntity<HttpHeaders> sut = this.sut.authenticate(user);
 
         assertThat(sut).isNotNull();
         assertThat(sut.getBody()).isNull();
@@ -74,8 +86,12 @@ class BoxChampAuthenticationServiceTest {
                 .ok()
                 .body(""));
 
+        when(boxChampProperties.getQueryParamEmail()).thenReturn(QUERY_PARAM_EMAIL);
+        when(boxChampProperties.getQueryParamPassword()).thenReturn(QUERY_PARAM_PASSWORD);
+        when(boxChampProperties.getLoginPath()).thenReturn(LOGIN_PATH);
+
         assertThatExceptionOfType(BadCredentialsException.class)
-                .isThrownBy(() -> loginService.authenticate(new User()))
+                .isThrownBy(() -> sut.authenticate(new User()))
                 .withMessageContaining("could not be authenticated");
     }
 }
